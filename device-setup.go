81fd6a6be39821dc96492ac587b8da8d659ba209
GP <giampaolo@cacophony.org.nz>
2024-04-10 10:20:56 +1200

    add trap type to device setup (#43)
    * add trap type to device setup
    
    * trap size
    
    ---------
    
    Co-authored-by: gferraro <g.ferraro22@gmail.com>

--- device-setup.go
+++ device-setup.go
@@ -27,7 +27,10 @@ func init() {
 }
 
 type DeviceSetup struct {
-	ir bool
+	IR bool `mapstructure:"ir"`
+
+	// S or L ( for small trap or large trap)
+	TrapSize string `mapstructure:"trap-size"`
 }
 
 func deviceSetupMapToStruct(m map[string]interface{}) (interface{}, error) {
