f1b9bad9ee0991d896a75469d42fede8fb253c82
Cameron <cameron.ryan.pears@gmail.com>
2025-08-08 12:19:47 +1200

    Merge binaries to reduce .deb size

---
+++ cmd/cacophony-config-util/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	cacophonyconfig "github.com/TheCacophonyProject/go-config/internal/cacophony-config"
+	cacophonyconfigsync "github.com/TheCacophonyProject/go-config/internal/cacophony-config-sync"
+	"github.com/sirupsen/logrus"
+)
+
+var log *logrus.Logger
+
+func main() {
+	err := runMain()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runMain() error {
+	if len(os.Args) < 2 {
+		log.Info("Usage: tool <subcommand> [args]")
+		return fmt.Errorf("no subcommand given")
+	}
+
+	sub := os.Args[1]
+	args := os.Args[2:]
+
+	var err error
+	switch sub {
+	case "sync":
+		err = cacophonyconfigsync.Run(args)
+	case "config":
+		err = cacophonyconfig.Run(args)
+	default:
+		err = fmt.Errorf("unknown subcommand: %s", sub)
+	}
+
+	return err
+}
